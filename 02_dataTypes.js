86480bf8ee316e63ee9a26da46648f098c80774b
jtejas01 <91519158+jtejas01@users.noreply.github.com>
2024-06-12 00:22:31 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"// treat all java script code as a newer version
+//alert(3 + 3) we are using node js not browser
+// ecma scripts orignal documentation standerds
+// but to learn we will use mdn documentation
+let age = 18
+let name = "tejas"
+let isLoggedIn = false
+
+
+//---------data types------------
+//number --> 2 
+//bigInt
+//boolean
+//string ""
+//null -> Stand Alone Value
+//undefined 
+//Symbol -> unique Value 
+
+//------------object---------------
+console.log(typeof(age))
+console.log(typeof null)//type is object
+console.log(typeof undefined)
+console.log(typeof name)
+console.log(typeof isLoggedIn)
