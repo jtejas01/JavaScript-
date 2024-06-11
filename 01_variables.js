a6e7148a8744e0c30f30f19d62d9617672b6c5d0
jtejas01 <91519158+jtejas01@users.noreply.github.com>
2024-06-12 00:22:02 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123
+let accountEmail = "abc@123"
+var acccountPassword = "1234"
+accountCity = "pune"
+let accountStatus;
+//accountId = 2
+accountEmail = "xyz@gmail.com"
+accountPassword = "4321"
+accountCity = "ahilyaNagar"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStatus])
