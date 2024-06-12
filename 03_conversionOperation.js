cc9baf17d11b2f9d50eab0aac9b0a648352d3e76
jtejas01 <91519158+jtejas01@users.noreply.github.com>
2024-06-12 11:06:09 +0530

    Add files via upload

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,28 +1,28 @@
-let score = 33
-console.log(typeof score)
-score = "33"
-console.log(typeof score)
-valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let score = 33
+// console.log(typeof score)
+// score = "33"
+// console.log(typeof score)
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-score = "33abc"
-console.log(typeof score)
-valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log("value "+valueInNumber);
+// score = "33abc"
+// console.log(typeof score)
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log("value "+valueInNumber);
 
-score = null
-console.log(typeof score)
-valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// score = null
+// console.log(typeof score)
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
-score = undefined
-console.log(typeof score)
-valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// score = undefined
+// console.log(typeof score)
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 // "33" -> 33
 // "33abc"->Nan
 //true -> 1;false ->0
@@ -30,3 +30,28 @@ console.log(valueInNumber);
 //1=> true
 // empty string => false 
 //"tejas "=> true 
+//************ operations ******************
+
+// let val = 3;
+// let negvalue = -val;
+// console.log(negvalue)
+
+// console.log(2 + 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 ** 3)
+// console.log(2 % 3)
+
+// let str1 ="hello"
+// let str2 = " tejas"
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(3+2+"1");
+
+// console.log(+true);
+// console.log(+"")
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+
