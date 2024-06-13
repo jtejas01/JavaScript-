67ae2399fc5170c866753dcace674b941a044b7d
jtejas01 <91519158+jtejas01@users.noreply.github.com>
2024-06-13 19:09:36 +0530

    Add files via upload

---
+++ 07_numsAndMaths.js
@@ -0,0 +1,23 @@
+const score = 100
+//console.log(score);
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const anotherNum = 23.8966
+//console.log(anotherNum.toPrecision(3));
+const otherNum = 123.8966
+//console.log(otherNum.toPrecision(3));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-In'));
+
+//**********************Maths *******************/
+console.log(Math.random());
+console.log((Math.round(Math.random()*10) + 1));
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
